34967060bc8c75c6acdbc6b0d23463d2597e3325
Yosf96633 <yosf96633@gmail.com>
2024-09-05 23:01:48 +0500

    ahh

--- src/Components/DashBoard/DashHero.jsx
+++ src/Components/DashBoard/DashHero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {Cards} from '../index'
 const DashHero = () => {
   return (
-    <div className=' h-[100vh] flex flex-col justify-center items-center gap-2'>
+    <div className='flex flex-col justify-center items-center gap-2'>
          <Cards/>
     </div>
   )
